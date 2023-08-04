29eac062b724e0454ae17284ad94f312aa1a464b
BKRamirezHuertas <ramirezhb3@gmail.com>
2023-08-04 02:57:06 -0500

    Avance mod5

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -64,7 +64,7 @@ function agregarNumeros(arrayOfNums) {
    let suma = 0
    for (let i= 0; i < arrayOfNums.length; i++) {
       suma += arrayOfNums[i]; }
-         return suma
+         return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
